f7687e6f065ebcf29d273e365567fc11356c139c
lepovica <lepo333@abv.bg>
2015-05-05 12:12:43 +0300

    fix identation

--- FuzzySet.js
+++ FuzzySet.js
@@ -1,21 +1,22 @@
 module.exports = function FuzzySet(repValue) {
     this._repValue = repValue;
     this._DOM = 0.0;
+
     this.getRepValue = function() {
         return this._repValue;
-    }
+    };
     this.clearDOM = function() {
         this._DOM = 0.0;
-    }
+    };
     this.setDOM = function(value) {
         this._DOM = value;
-    }
+    };
     this.getDOM = function() {
         return this._DOM;
-    }
+    };
     this.ORwithDOM = function(value) {
         if (this._DOM < value) {
             this.setDOM(value);
         }
-    }
-}
+    };
+};
