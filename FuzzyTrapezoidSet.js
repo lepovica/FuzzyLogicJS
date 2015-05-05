84bc2ae65ca71c78a2ddde69619ee37faace5bd6
lepovica <lepo333@abv.bg>
2015-05-05 16:17:55 +0300

    add trapezoid set

---
+++ FuzzyTrapezoidSet.js
@@ -0,0 +1,39 @@
+var FuzzySet = require('./FuzzySet');
+
+module.exports = function FuzzyTrapezoidSet(name, leftOffset, leftPeakPoint, rightPeakPoint, rightOffset) {
+    FuzzySet.call(this, name, (((leftPeakPoint + leftOffset) + leftPeakPoint) / 2));
+    this._leftPeakPoint = leftPeakPoint;
+    this._rightPeakPoint = rightPeakPoint;
+    this._leftOffset = leftOffset;
+    this._rightOffset = rightOffset;
+};
+
+module.exports.prototype = Object.create(FuzzySet.prototype);
+
+module.exports.prototype.calculateDOM = function(value) {
+    if ((this._leftOffset === 0.0) && (value === this._leftPeakPoint)) {
+        return 1.0;
+    }
+    if ((value <= this._leftPeakPoint) && (value > (this._leftPeakPoint - this._leftOffset))) {
+        var grad = 1.0 / this._leftOffset;
+        return grad * (value - (this._leftPeakPoint - this._leftOffset));
+    } else {
+        if ((value > this._leftPeakPoint) && (value < this._rightPeakPoint)) {
+            return 1.0;
+        } else {
+            if ((this._rightOffset === 0.0) && (value === this._rightPeakPoint)) {
+                return 1.0;
+            }
+            if ((value >= this._rightPeakPoint) && (value < (this._rightPeakPoint + this._rightOffset))) {
+                var grad = 1.0 / this._rightOffset;
+                return grad * (value - (this._rightPeakPoint - this._rightOffset));
+            } else {
+                if (value < this._rightPeakPoint) {
+                    return 1.0;
+                } else {
+                    return 0.0;
+                }
+            }
+        }
+    }
+};
--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 var FuzzyTerm = require('./FuzzyTerm');
-var FuzzyLeftShoulderSet = require('./FuzzyLeftShoulderSet');
+var FuzzyRightShoulderSet = require('./FuzzyRightShoulderSet');
 var FuzzyRule = require('./FuzzyRule');
 
-var set = new FuzzyLeftShoulderSet("close", 20, 50, 30);
+var set = new FuzzyRightShoulderSet("close", 20, 50, 30);
 
-set.setDOM(set.calculateDOM(39));
+set.setDOM(set.calculateDOM(29));
 
 console.log(set);
 
-var set2 = new FuzzyLeftShoulderSet("ammo", 15, 15, 35);
-set2.setDOM(set2.calculateDOM(18));
+var set2 = new FuzzyRightShoulderSet("ammo", 15, 15, 35);
+set2.setDOM(set2.calculateDOM(10));
 
 console.log(set2);
 
-var set3 = new FuzzyLeftShoulderSet("desirable", 20, 100, 0);
+var set3 = new FuzzyRightShoulderSet("desirable", 20, 100, 0);
 console.log(set3);
 
 var ant = new FuzzyTerm(set).fzAndWith(new FuzzyTerm(set2));
@@ -26,11 +26,11 @@ var rule2 = new FuzzyRule(ant2, con);
 console.log("raw : ")
 console.log(rule);
 console.log(rule2);
-rule2.calculate();
-console.log("calculate 2 : ");
-console.log(rule);
-console.log(rule2);
 rule.calculate();
 console.log("calculate 1 : ");
+console.log(rule);
+console.log(rule2);
+rule2.calculate();
+console.log("calculate 2 : ");
 console.log(rule);
 console.log(rule2);
