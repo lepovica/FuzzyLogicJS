99ac58eb3c334031969504496add2ac2a3f4519d
lepovica <lepo333@abv.bg>
2015-05-05 15:30:59 +0300

    left shoulder set inherit fuzzy set and tests

--- FuzzyLeftShoulderSet.js
+++ FuzzyLeftShoulderSet.js
@@ -1,43 +1,26 @@
-module.exports = function (name, leftOffset, peakPoint, rightOffset) {
-	var core = {
-		_repValue : (((peakPoint - leftOffset) + peakPoint) / 2), // TODO : should check this
-		_name : name,
-		_DOM : 0.0,
-		_peakPoint : peakPoint,
-		_leftOffset : leftOffset,
-		_rightOffset : rightOffset,
-		calculateDOM : function(value) {
-			if ( (rightOffset === 0.0) && (value === peakPoint) ) {
-				return 1.0;
-			}
-			if ( (value >= peakPoint) && (value < (peakPoint - rightOffset))) {
-				var grad = 1.0 / rightOffset;
-				return grad * (value - (peakPoint - rightOffset));
-			} else {
-				if (value < peakPoint) {
-					return 1.0;
-				} else {
-					return 0.0;
-				}
-			}
-		},
-		clearDOM : function() {
-			core._DOM = 0.0;
-		},
-		setDOM : function(value) {
-			core._DOM = value;
-		},
-		getDOM : function(value) {
-			return core._DOM;
-		},
-		getName : function() {
-			return core._name;
-		},
-		ORwithDOM : function(value) {
-			if(core._DOM < value) {
-				core.setDOM(value);
-			}
-		}
-	}
-	return core;
-}
+var FuzzySet = require('./FuzzySet');
+
+module.exports = function FuzzyLeftShoulderSet(name, leftOffset, peakPoint, rightOffset) {
+    FuzzySet.call(this, name, (((peakPoint - leftOffset) + peakPoint) / 2));
+    this._peakPoint = peakPoint;
+    this._leftOffset = leftOffset;
+    this._rightOffset = rightOffset;
+};
+
+module.exports.prototype = Object.create(FuzzySet.prototype);
+
+module.exports.prototype.calculateDOM = function(value) {
+    if ((this._rightOffset === 0.0) && (value === this._peakPoint)) {
+        return 1.0;
+    }
+    if ((value >= this._peakPoint) && (value < (this._peakPoint - this._rightOffset))) {
+        var grad = 1.0 / this._rightOffset;
+        return grad * (value - (this._peakPoint - this._rightOffset));
+    } else {
+        if (value < this._peakPoint) {
+            return 1.0;
+        } else {
+            return 0.0;
+        }
+    }
+};
