b5b773aa99eaa6e59a762c9006654484e0c97f40
lepovica <lepo333@abv.bg>
2015-05-05 15:07:03 +0300

    finish tests for rules

--- index.js
+++ index.js
@@ -7,3 +7,30 @@ var set = new FuzzyTriangleSet("close", 20, 50, 30);
 set.setDOM(set.calculateDOM(39));
 
 console.log(set);
+
+var set2 = new FuzzyTriangleSet("ammo", 15, 15, 35);
+set2.setDOM(set2.calculateDOM(18));
+
+console.log(set2);
+
+var set3 = new FuzzyTriangleSet("desirable", 20, 100, 0);
+console.log(set3);
+
+var ant = new FuzzyTerm(set).fzAndWith(new FuzzyTerm(set2));
+var con = new FuzzyTerm(set3);
+var ant2 = new FuzzyTerm(set).fzOrWith(new FuzzyTerm(set2));
+
+var rule = new FuzzyRule(ant, con);
+var rule2 = new FuzzyRule(ant2, con);
+
+console.log("raw : ")
+console.log(rule);
+console.log(rule2);
+rule.calculate();
+console.log("calculate 1 : ");
+console.log(rule);
+console.log(rule2);
+rule2.calculate();
+console.log("calculate 2 : ");
+console.log(rule);
+console.log(rule2);
